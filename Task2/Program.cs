dd9c4edee0a5713c9c4e99b3f2184e1277e09afc
Olga <frau.tsaregorodtseva2016@gmail.com>
2023-01-24 10:23:56 +0300

    Координаты

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+System.Console.WriteLine("Введите координаты первой точки");
+int x1 = Convert.ToInt32 (Console.ReadLine ());
+int y1 = Convert.ToInt32 (Console.ReadLine ());
+int z1 = Convert.ToInt32 (Console.ReadLine ());
+
+System.Console.WriteLine("Введите координаты второй точки");
+int x2 = Convert.ToInt32 (Console.ReadLine ());
+int y2 = Convert.ToInt32 (Console.ReadLine ());
+int z2 = Convert.ToInt32 (Console.ReadLine ());
+
+System.Console.WriteLine("Расстояние между двумя точками");
+System.Console.Write(Math.Round(Math.Pow((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) *1.0), 0.5),2));
